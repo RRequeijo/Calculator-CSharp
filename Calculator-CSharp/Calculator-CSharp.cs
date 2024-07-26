7605d2558ee15570ca46c85dc55be1b59bb6b44a
RRequeijo <al79138@alunos.utad.pt>
2024-07-26 21:33:16 +0100

    Calc Fase 3

--- Calculator-CSharp/Calculator-CSharp.cs
+++ Calculator-CSharp/Calculator-CSharp.cs
@@ -1,144 +1,292 @@
-﻿namespace Calculadora
+﻿namespace Calculator
 {
     using System;
 
     /// <summary>
-    /// <see cref="Calculadora"/>
+    /// <see cref="Calculator"/>
     /// </summary>
-    internal class Calculadora
+    internal class Calculator
     {
+        public static double Cube(double a)
+        {
+            return a * a * a;
+        }
+
+        public static double CubeRoot(double a)
+        {
+            if (a < 0)
+            {
+                return 0;
+            }
+            return Math.Cbrt(a);
+        }
+
+        public static int Factorial(int a)
+        {
+            int total = a;
+
+            for (int i = a - 1; i >= 1; i--)
+            {
+                total *= i;
+            }
+            return total;
+        }
+
         /// <summary>
         /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
-            Console.WriteLine("Calculadora");
-            Console.WriteLine("+  Soma \n-  Subtracao \n/  Divisao \n*  Multiplicacao \nr  Resto \n=  Comparar");
-            Console.WriteLine("Digita o simbolo da operação desejada: \n");
-            char operacao = Console.ReadLine()[0];
+            Console.WriteLine("Calculator");
+            Console.WriteLine("+  Sum \n-  Subtraction \n/  Division \n*  Multiplication \nr  Rest \n=  Compare \ne  Even \np  Prime \nf  Factorial \ns  Square \nc  Cube \nS  SquareRoot \nC  CubeRoot");
+            Console.WriteLine("Enter the symbol of the desired operation:");
+            char operation = Console.ReadLine()[0];
 
-            Console.WriteLine("Valor 1:");
-            double valor1 = Convert.ToDouble(Console.ReadLine());
+            int value;
+            double value1 = 0;
+            double value2 = 0;
 
-            Console.WriteLine("Valor 2:");
-            double valor2 = Convert.ToDouble(Console.ReadLine());
+            double result = 0;
 
-            double resultado = 0;
-
-            switch (operacao)
+            switch (operation)
             {
                 case '+':
-                    resultado = Somar(valor1, valor2);
-                    Console.WriteLine($"Resultado da Soma de {valor1} por {valor2}: {resultado}");
+                    Console.WriteLine("value 1:");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("value 2:");
+                    value2 = Convert.ToDouble(Console.ReadLine());
+                    result = Sum(value1, value2);
+                    Console.WriteLine($"Result of the Sum from {value1} by {value2}: {result}");
                     break;
 
                 case '-':
-                    resultado = Subtrair(valor1, valor2);
-                    Console.WriteLine($"Resultado da Subtracao de {valor1} por {valor2}: {resultado}");
+                    Console.WriteLine("value 1:");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("value 2:");
+                    value2 = Convert.ToDouble(Console.ReadLine());
+                    result = Subtract(value1, value2);
+                    Console.WriteLine($"result of Subtraction of {value1} by {value2}: {result}");
                     break;
 
                 case '/':
-                    resultado = Dividir(valor1, valor2);
-                    Console.WriteLine($"Resultado da Divisao de {valor1} por {valor2}: {resultado}");
+                    Console.WriteLine("value 1:");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("value 2:");
+                    value2 = Convert.ToDouble(Console.ReadLine());
+                    result = Divide(value1, value2);
+                    Console.WriteLine($"result of Dividing {value1} by {value2}: {result}");
                     break;
 
                 case '*':
-                    resultado = Multiplicar(valor1, valor2);
-                    Console.WriteLine($"Resultado da Multiplicacao de {valor1} por {valor2}: {resultado}");
+                    Console.WriteLine("value 1:");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("value 2:");
+                    value2 = Convert.ToDouble(Console.ReadLine());
+                    result = Multiply(value1, value2);
+                    Console.WriteLine($"result of Multiplication of {value1} by {value2}: {result}");
                     break;
 
                 case 'r':
-                    resultado = Resto(valor1, valor2);
-                    Console.WriteLine($"Resto da divisao de {valor1} por {valor2}: {resultado}");
+                    Console.WriteLine("value 1:");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("value 2:");
+                    value2 = Convert.ToDouble(Console.ReadLine());
+
+                    result = Rest(value1, value2);
+                    Console.WriteLine($"Rest of the division of {value1} by {value2}: {result}");
                     break;
 
                 case '=':
-                    Comparar(valor1, valor2);
+                    Console.WriteLine("value 1:");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("value 2:");
+                    value2 = Convert.ToDouble(Console.ReadLine());
+                    Compare(value1, value2);
+                    break;
+
+                case 'e':
+                    Console.WriteLine("value:");
+                    value = Convert.ToInt32(Console.ReadLine());
+                    VerifyEven(value);
+                    if (VerifyEven(value) == true)
+                    {
+                        Console.WriteLine("The number is even.");
+                    }
+                    if (VerifyEven(value) == false)
+                    {
+                        Console.WriteLine("The number is odd.");
+                    }
+                    break;
+
+                case 'p':
+                    Console.WriteLine("value:");
+                    value = Convert.ToInt32(Console.ReadLine());
+                    VerifyPrime(value);
+                    if (VerifyEven(value) == false)
+                    {
+                        Console.WriteLine("The number is prime.");
+                    }
+                    if (VerifyEven(value) == true)
+                    {
+                        Console.WriteLine("The number is not prime.");
+                    }
+                    break;
+
+                case 'f':
+                    Console.WriteLine("value:");
+                    value = Convert.ToInt32(Console.ReadLine());
+                    result = Factorial(value);
+                    Console.WriteLine($"The value of {value} in Factorial is {result}");
+                    break;
+
+                case 's':
+                    Console.WriteLine("value:");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+                    result = Square(value1);
+                    Console.WriteLine($"The frame of {value1} is {result}");
+                    break;
+
+                case 'c':
+                    Console.WriteLine("value:");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+                    result = Cube(value1);
+                    Console.WriteLine($"The Cube of {value1} is {result}");
+                    break;
+
+                case 'S':
+                    Console.WriteLine("value:");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+                    result = SquareRoot(value1);
+                    Console.WriteLine($"The square root of {value1} is {result}");
+                    break;
+
+                case 'C':
+                    Console.WriteLine("value:");
+                    value1 = Convert.ToDouble(Console.ReadLine());
+                    result = CubeRoot(value1);
+                    Console.WriteLine($"The cube root of {value1} is {result}");
                     break;
 
                 default:
-                    {
-                        Console.WriteLine("Insira uma operação válida");
-                        break;
-                    }
+                    Console.WriteLine("Enter a valid operation");
+                    break;
             }
         }
 
+        public static double Square(double a)
+        {
+            return a * a;
+        }
+
+        public static double SquareRoot(double a)
+        {
+            if (a < 0)
+            {
+                return 0;
+            }
+            return Math.Sqrt(Math.Sqrt(a));
+        }
+
+        public static bool VerifyEven(int a)
+        {
+            return a % 2 == 0;
+        }
+
+        public static bool VerifyPrime(int a)
+        {
+            if (a <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         /// <summary>
-        /// Comparars the specified a.
+        /// Compares the specified a.
         /// </summary>
         /// <param name="a">a.</param>
         /// <param name="b">The b.</param>
-        private static void Comparar(double a, double b)
+        private static void Compare(double a, double b)
         {
             if (a > b)
             {
-                Console.WriteLine($"{a} é maior que {b}");
+                Console.WriteLine($"{a} is greater than {b}");
             }
             if (a < b)
             {
-                Console.WriteLine($"{a} é menor {b}");
+                Console.WriteLine($"{a} is smaller {b}");
             }
-            if (a == b)
-            {
-                Console.WriteLine($"{a} é igual a {b}");
-            }
+            Console.WriteLine($"{a} is equal to {b}");
         }
 
         /// <summary>
-        /// Dividirs the specified a.
+        /// Divides the specified a.
         /// </summary>
         /// <param name="a">a.</param>
         /// <param name="b">The b.</param>
         /// <returns></returns>
-        private static double Dividir(double a, double b)
+        private static double Divide(double a, double b)
         {
             return a / b;
         }
 
         /// <summary>
-        /// Multiplicars the specified a.
+        /// Multiplys the specified a.
         /// </summary>
         /// <param name="a">a.</param>
         /// <param name="b">The b.</param>
         /// <returns></returns>
-        private static double Multiplicar(double a, double b)
+        private static double Multiply(double a, double b)
         {
             return a * b;
         }
 
         /// <summary>
-        /// Restoes the specified a.
+        /// Restes the specified a.
         /// </summary>
         /// <param name="a">a.</param>
         /// <param name="b">The b.</param>
         /// <returns></returns>
-        private static double Resto(double a, double b)
+        private static double Rest(double a, double b)
         {
             return a % b;
         }
 
         /// <summary>
-        /// Somars the specified a.
+        /// Subtracts the specified a.
         /// </summary>
         /// <param name="a">a.</param>
         /// <param name="b">The b.</param>
         /// <returns></returns>
-        private static double Somar(double a, double b)
+        private static double Subtract(double a, double b)
         {
-            return a + b;
+            return a - b;
         }
 
         /// <summary>
-        /// Subtrairs the specified a.
+        /// Sums the specified a.
         /// </summary>
         /// <param name="a">a.</param>
         /// <param name="b">The b.</param>
         /// <returns></returns>
-        private static double Subtrair(double a, double b)
+        private static double Sum(double a, double b)
         {
-            return a - b;
+            return a + b;
         }
     }
 }
