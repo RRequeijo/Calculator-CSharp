fd65ea1eff52f0b270e67ccaf42012fd1b1408f6
RRequeijo <al79138@alunos.utad.pt>
2024-07-26 02:09:23 +0100

    calc fase2

--- Calculator-CSharp/Calculator-CSharp.cs
+++ Calculator-CSharp/Calculator-CSharp.cs
@@ -4,10 +4,6 @@
 
     internal class Calculadora
     {
-        /// <summary>
-        /// Defines the entry point of the application.
-        /// </summary>
-        /// <param name="args">The arguments.</param>
         public static void Main(string[] args)
         {
             Console.WriteLine("Calculator");
